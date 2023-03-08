8a2f477327ae1aa49ada45f300e103898b4cb0a5
tomasamori <tomas.amori@gmail.com>
2023-03-08 10:19:42 -0300

    requiring mongoose and models

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+// MongoDB connection
+
 require('./connection');
+
+// Requiring mongoose and models
+
+const mongoose = require('mongoose');
+const Author = require('./models/Author');
+const Book = require('./models/Book');
